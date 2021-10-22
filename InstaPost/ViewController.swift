319e538dd6e4c03fd7d1897f1fffa715a783cc24
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-10-22 20:49:48 +0700

    Initial Commit

---
+++ InstaPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPost
+//
+//  Created by Gilbert Nicholas on 22/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
